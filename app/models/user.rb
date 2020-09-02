723f7d65da5330cd036902cd3cd095c2c518d92a
Daniel <danielnottingham.ti@gmail.com>
2020-09-02 13:51:32 -0300

    search corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :name, :phone, presence: true
   validates :name, format: { with: /\A[a-zA-Z]+\z/ }
   validates :phone, numericality: { only_integer: true }
-  scope :search_by_name, ->(query) { where('name ilike ?', "#{query}%") }
+  scope :search_by_name, ->(query) { where('name ilike ?', "%#{query}%") }
 end
