864b806db5c036195d39fb8cefe6cd93b2779b52
Daniel <danielnottingham.ti@gmail.com>
2020-09-07 14:33:00 -0300

    initial show error msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to action: 'index'
+    if @user.save
+      redirect_to action: 'index'
+    else
+      render :new
+    end
   end
 
   def edit
