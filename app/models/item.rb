0fda0c457c362aec6e394d5ff89ca32bd3694b2d
Daniel <danielnottingham.ti@gmail.com>
2020-09-10 11:13:17 -0300

    value validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   validates :key, :value, presence: true
-  validates :key, format: { with: /\A[a-zA-Z]+\z/ }
-  validates :value, numericality: { only_integer: true }
+  validates :key, :value, format: { with: /\A[a-zA-Z]+\z/ }
   scope :search_by_key, ->(query) { where('key ilike ?', "%#{query}%") }
 end
